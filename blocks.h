3c4e6dfe3e1dc3d9d7d6f02218ed9eec7542cc5f
Vivek Sundaray <viveksundaray123@outlook.com>
2021-10-07 08:16:06 +0530

    Excluded config.def.h

--- blocks.h
+++ blocks.h
@@ -1,7 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"🔼: ", "upload-speed.sh",					2,		0},
+    {"📶: ", "iwgetid -r",					0,		0},
+    {"🔽: ", "/data/vivek/extra/scripts/download-speed.sh",					2,		0},
+    {"🔼: ", "/data/vivek/extra/scripts/upload-speed.sh",					2,		0},
     {"🐧: ", "uname -r",					0,		0},
 	{"📦: ", "pacman -Q | wc -l",					600,		0},
 	{"📅: ", "date '+%d-%m %H:%M'",					60,		0},
