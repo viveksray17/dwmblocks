eb19af29f41682b92e2297f9ab3782a7fb5bfc5a
Vivek Sundaray <viveksundaray123@outlook.com>
2021-12-20 07:37:18 +0530

    Added sample file so that it builds properly

---
+++ blocks.def.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"📶 ", "$HOME/.scripts/get-ssid",	0,		0},
+    {"", "$HOME/.scripts/get-volume",   0,		10},
+	{"📅 ", "date '+%d-%m-%Y %H:%M'",   60,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 3;
